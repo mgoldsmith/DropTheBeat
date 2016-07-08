bfbf7b82b4379ad5dc9cd5cb6dcf5f63aa1da692
Jeremy Monson <monson.jeremy@gmail.com>
2016-07-08 11:43:53 -0700

    Initial Commit

---
+++ DropTheBeat/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DropTheBeat
+//
+//  Created by Jeremy Monson on 7/2/16.
+//  Copyright © 2016 Jeremy Monson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
